9c9c98bdfe35757ad699f433ecd6c20c8d1ace80
Galen Guyer <galen@galenguyer.com>
2020-11-08 23:20:20 -0500

    set up initializes test

--- src/lib.rs
+++ src/lib.rs
@@ -10,10 +10,7 @@ impl Stack {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#[test]
+fn initializes() {
+    let _ = Stack::new(vec![0]);
 }
